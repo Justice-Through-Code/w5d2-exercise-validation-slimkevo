c3b00718a52502eeccf5b30dde288db8a69aa88f
slimkevo <kevinkamerling2021@gmail.com>
2023-10-05 15:08:44 -0400

    final

--- validation.py
+++ validation.py
@@ -19,8 +19,8 @@ def validate_user_input():
 
 # 1.2 TODO: Once you are done, uncomment the two lines below to ensure that your code works as expected
 
-user_number = validate_user_input() 
-print(f'The number the user entered is {user_number}.')
+# user_number = validate_user_input() 
+# print(f'The number the user entered is {user_number}.')
 
 
 # 2.1 TODO: Create a function called print_tenth_item that will
